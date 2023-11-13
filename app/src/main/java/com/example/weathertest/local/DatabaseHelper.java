dac9a8b2306c891de8b7e608ff3da9dfa57021b2
Anastasia Vilmovskaya <a.vilmovskaya@gmail.com>
2023-11-13 22:33:01 +0300

    Подключение базы данных SQLite

---
+++ app/src/main/java/com/example/weathertest/local/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.example.weathertest.local;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.weathertest.utils.Constants;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, Constants.DB_NAME, null, Constants.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(Constants.TABLE_STRUCTURE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL(Constants.DROP_TABLE);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/weathertest/local/DatabaseManager.java
@@ -0,0 +1,46 @@
+package com.example.weathertest.local;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.weathertest.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseManager {
+    private DatabaseHelper helper;
+    private SQLiteDatabase db;
+
+
+    public DatabaseManager(Context context) {
+        helper = new DatabaseHelper(context);
+    }
+
+    public void openDb(){
+        db = helper.getWritableDatabase();
+    }
+    public void insertIntoDb(String city){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Constants.CITY, city);
+        db.insert(Constants.TABLE_NAME, null, contentValues);
+    }
+
+    public List<String> getFromDb(){
+        List<String> tempList = new ArrayList<>();
+        Cursor cursor = db.query(Constants.TABLE_NAME, null, null, null, null,null,null);
+        while(cursor.moveToNext()){
+            @SuppressLint("Range") String city = cursor.getString(cursor.getColumnIndex(Constants.CITY));
+            tempList.add(city);
+        }
+        cursor.close();
+        return tempList;
+    }
+
+    public void closeDb(){
+        helper.close();
+    }
+}
