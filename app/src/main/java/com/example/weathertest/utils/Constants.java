d6edea2265c04cd9a6ed62a4e5b759e30e30930e
Anastasia Vilmovskaya <a.vilmovskaya@gmail.com>
2023-11-13 22:33:41 +0300

    Ссылки, ендпоинты и константы базы данных

---
+++ app/src/main/java/com/example/weathertest/utils/Constants.java
@@ -0,0 +1,24 @@
+package com.example.weathertest.utils;
+
+public class Constants {
+
+    public static final String URL = "https://api.openweathermap.org/";
+    public static final String CURRENT_WEATHER_END_POINT = "data/2.5/weather";
+
+    public static final String APIKEY = "9ed3426810f8174bf0b02c62d53eef00";
+
+    public static final String TABLE_NAME = "cities_table";
+
+    public static final String _ID = "_id";
+
+    public static final String CITY = "city";
+
+    public static final String DB_NAME = "database.db";
+
+    public static final int DB_VERSION = 1;
+
+    public static final String TABLE_STRUCTURE = "CREATE TABLE IF NOT EXISTS " +
+            TABLE_NAME + " (" + _ID + " INTEGER PRIMARY KEY, " + CITY + " TEXT)";
+    public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+}
