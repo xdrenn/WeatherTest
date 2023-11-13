a79a461ebfbf085fb1dce1f0dc83a148c470a66b
Anastasia Vilmovskaya <a.vilmovskaya@gmail.com>
2023-11-13 22:34:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weathertest/presentation/di/WeatherApplication.java
@@ -0,0 +1,9 @@
+package com.example.weathertest.presentation.di;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class WeatherApplication extends Application {
+}
