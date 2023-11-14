dec709af701aba7ba39d0df321fda1d5afe665d6
Anastasia Vilmovskaya <a.vilmovskaya@gmail.com>
2023-11-14 21:04:51 +0300

    Добавила кнопку "назад", во все TextView добавила соответствующие данные от Api, добавила получение значка погоды в настоящее время

--- app/src/main/java/com/example/weathertest/presentation/ui/ForecastFragment.java
+++ app/src/main/java/com/example/weathertest/presentation/ui/ForecastFragment.java
@@ -11,9 +11,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.weathertest.R;
 import com.example.weathertest.databinding.FragmentForecastBinding;
 import com.example.weathertest.presentation.mvvm.WeatherViewModel;
 import com.example.weathertest.utils.Constants;
+import com.squareup.picasso.Picasso;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -24,8 +26,13 @@ public class ForecastFragment extends Fragment {
     private FragmentForecastBinding binding;
 
     private WeatherViewModel weatherViewModel;
+    private SearchFragment fragment;
 
-    private Bundle bundle;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fragment = new SearchFragment();
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -47,22 +54,39 @@ public class ForecastFragment extends Fragment {
         String cityFromSearch = getArguments().getString("cityFromSearch");
 
 
+
         if(cityFromDb != null) {
             requestWeatherByCity(cityFromDb);
         }
         if(cityFromSearch != null){
             requestWeatherByCity(cityFromSearch);
         }
-        requestWeatherByCoord(lat, lon);
+        if(cityFromSearch == null && cityFromDb == null) {
+            requestWeatherByCoord(lat, lon);
+        }
+
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getParentFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, fragment).commit();
+            }
+        });
     }
 
     @SuppressLint("SetTextI18n")
     public void requestWeatherByCity(String city) {
         weatherViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
         weatherViewModel.initWeatherRequestCity(city, Constants.APIKEY);
-        weatherViewModel.getWeatherByCity().observe(getViewLifecycleOwner(), string -> {
-                binding.temperature.setText(string.getMain().getTemp().toString());
-                binding.city.setText(string.getName());
+        weatherViewModel.getWeatherByCity().observe(getViewLifecycleOwner(), weather -> {
+                binding.temperature.setText(weather.getMain().getTemp().toString());
+                binding.city.setText(weather.getName());
+                weather.getWeather().forEach(condition -> binding.condition.setText(condition.getMain()));
+                weather.getWeather().forEach(icon -> Picasso.get().load(Constants.ICON_URL + icon.getIcon() + ".png").into(binding.weatherIcon));
+                binding.minTemp.setText("L:" + weather.getMain().getTempMin().toString());
+                binding.maxTemp.setText("H:" + weather.getMain().getTempMax().toString());
+                binding.tempFeelsLike.setText(weather.getMain().getFeelsLike().toString());
+                binding.humidity.setText(weather.getMain().getHumidity() + "%");
+                binding.windSpeed.setText(weather.getWind().getSpeed().toString() + "m/s");
         });
     }
 
@@ -70,8 +94,16 @@ public class ForecastFragment extends Fragment {
     public void requestWeatherByCoord(double lat, double lon) {
         weatherViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
         weatherViewModel.initWeatherRequestCoord(lat, lon, Constants.APIKEY);
-        weatherViewModel.getWeatherByCoord().observe(getViewLifecycleOwner(), string ->
-                binding.condition.setText(string.getMain().getTemp().toString()));
-
+        weatherViewModel.getWeatherByCoord().observe(getViewLifecycleOwner(),  weather -> {
+            binding.temperature.setText(weather.getMain().getTemp().toString());
+            binding.city.setText(weather.getName());
+            weather.getWeather().forEach(condition -> binding.condition.setText(condition.getMain()));
+            weather.getWeather().forEach(icon -> Picasso.get().load(Constants.ICON_URL + icon.getIcon() + ".png").into(binding.weatherIcon));
+            binding.minTemp.setText("L:" + weather.getMain().getTempMin().toString());
+            binding.maxTemp.setText("H:" + weather.getMain().getTempMax().toString());
+            binding.tempFeelsLike.setText(weather.getMain().getFeelsLike().toString());
+            binding.humidity.setText(weather.getMain().getHumidity() + "%");
+            binding.windSpeed.setText(weather.getWind().getSpeed().toString());
+        });
     }
 }
