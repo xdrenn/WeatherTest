9eba7f885d063ad8aeec6c33fd58789499bdee93
Anastasia Vilmovskaya <a.vilmovskaya@gmail.com>
2023-11-14 20:55:20 +0300

    Исправила структуру таблицы бд

--- app/src/main/java/com/example/weathertest/utils/Constants.java
+++ app/src/main/java/com/example/weathertest/utils/Constants.java
@@ -3,6 +3,8 @@ package com.example.weathertest.utils;
 public class Constants {
 
     public static final String URL = "https://api.openweathermap.org/";
+
+    public static final String ICON_URL = "https://openweathermap.org/img/w/";
     public static final String CURRENT_WEATHER_END_POINT = "data/2.5/weather";
 
     public static final String APIKEY = "9ed3426810f8174bf0b02c62d53eef00";
@@ -18,7 +20,7 @@ public class Constants {
     public static final int DB_VERSION = 1;
 
     public static final String TABLE_STRUCTURE = "CREATE TABLE IF NOT EXISTS " +
-            TABLE_NAME + " (" + _ID + " INTEGER PRIMARY KEY, " + CITY + " TEXT)";
+            TABLE_NAME + " (" + _ID + " INTEGER PRIMARY KEY," + CITY + " TEXT)";
     public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
 }
