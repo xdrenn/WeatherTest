5ea5f3ae0bce774930c2ebce28c2bdd2c977c3ea
Anastasia Vilmovskaya <a.vilmovskaya@gmail.com>
2023-11-17 14:43:41 +0300

    Добавила новый шрифт и цвета, поменяла иконки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,13 +35,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-fragment:2.7.5")
-    implementation("androidx.navigation:navigation-ui:2.7.5")
     val daggerVersion = "2.48.1"
     val retrofitVersion = "2.9.0"
     val okhttpVersion = "4.10.0"
     val picassoVersion = "2.8"
     val roomVersion = "2.6.0"
+    val rxJavaVersion = "3.1.8"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -49,12 +48,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
 
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
     implementation( "com.squareup.retrofit2:adapter-rxjava3:$retrofitVersion")
-
     implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
 
@@ -68,7 +68,6 @@ dependencies {
     implementation ("com.google.dagger:hilt-android:$daggerVersion")
     annotationProcessor ("com.google.dagger:hilt-android-compiler:$daggerVersion")
 
-
     //picasso
     implementation("com.squareup.picasso:picasso:$picassoVersion")
 
@@ -76,5 +75,6 @@ dependencies {
     implementation("androidx.room:room-runtime:$roomVersion")
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
 
-    implementation ("io.reactivex.rxjava3:rxjava:3.1.8")
+    //rxjava
+    implementation ("io.reactivex.rxjava3:rxjava:$rxJavaVersion")
 }
